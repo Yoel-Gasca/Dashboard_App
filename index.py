b7d73c3ff75423fe65e0697a9c743a569c71cd53
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2024-07-03 23:40:46 -0600

    Styless add

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
 import dash
 import dash_core_components as dcc 
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd 
 from dash.dependencies import Input,Output
 
-df = pd.read_csv('Dashboard_App\Data\german_credit.csv')
+df = pd.read_csv ('Dashboard_App\Data\german_credit.csv')
 print(df)
 
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div([
+    
+    html.Div([
+        html.H1('German Bank'),
+        html.Img(src='assets/img/BankAppLogo2.png')
+    ], className = 'banner'),
+])
+
+if __name__ == ('__main__'):
+    app.run_server(debug=True)
