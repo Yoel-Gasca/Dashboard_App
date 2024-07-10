2f2023f802e6e8b7f4637d070f14dc888fee493e
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2024-07-09 22:06:34 -0600

    Developing Graphics and Design Adjustments

--- index.py
+++ index.py
@@ -24,67 +24,88 @@ app.layout = html.Div([
 
      html.Div([
         html.Div([
-            html.P('Selecciona el historial del credito', className = 'fix_label', style={'color':'black', 'margin-top': '2px'}),
-            dcc.RadioItems(id = 'credit_history', 
-                            labelStyle = {'display': 'inline-block'},
-                            options = [
-                                {'label' : 'Critical account', 'value' : 'critical account/ other credits existing (not at this bank)'},
-                                {'label' : 'Existing credits', 'value' : 'existing credits paid back duly till now'},
-                                {'label' : 'No credits taken', 'value' : 'no credits taken/ all credits paid back duly'},
-                                {'label' : 'delay in paying', 'value': 'delay in paying off in the past'}
-                            ], value = 'historial_de_credito',
-                            style = {'text-aling':'center', 'color':'black'}, className = 'dcc_compon'),
-        ], className = 'create_container2 five columns', style = {'margin-bottom': '20px'}),
-    ], className = 'row flex-display'),
+            html.P('Selecciona el historial del crédito', className='fix_label', style={'color':'black', 'margin-top': '2px'}),
+            dcc.RadioItems(
+                id='credit_history',
+                labelStyle={'display': 'inline-block', 'margin-right': '20px'},
+                options=[
+                    {'label': 'Critical account', 'value': 'critical account/ other credits existing (not at this bank)'},
+                    {'label': 'Existing credits', 'value': 'existing credits paid back duly till now'},
+                    {'label': 'No credits taken', 'value': 'no credits taken/ all credits paid back duly'},
+                    {'label': 'Delay in paying', 'value': 'delay in paying off in the past'}
+                ],
+                value='existing credits paid back duly till now',
+                style={'text-align':'center', 'color':'black'}, 
+                className='dcc_compon'
+            ),
+        ], className='create_container2 five columns', style={'margin-bottom': '20px'}),
+    ], className='row flex-display'),
 
     html.Div([
         html.Div([
-            dcc.Graph(id = 'my_graph', figure = {})
-        ], className = 'create_container2 eight columns'),
+            dcc.Graph(id='my_graph', figure={})
+        ], className='create_container2'),
 
         html.Div([
-            dcc.Graph(id = 'pie_graph', figure = {})
-        ], className = 'create_container2 five columns')
-    ], className = 'row flex-display'),
+            dcc.Graph(id='pie_graph', figure={})
+        ], className='create_container2')
+    ], className='row flex-display'),
 
 ], id='mainContainer', style={'display':'flex', 'flex-direction':'column'})
 
 @app.callback(
-    Output('my_graph', component_property='figure'),
-    [Input('dosis-radioitems', component_property='value')])
-
+    Output('my_graph', 'figure'),
+    [Input('credit_history', 'value')]
+)
 def update_graph(value):
+    filtered_df = df[df['credit_history'] == value]
+    
+    fig = go.Figure()
 
-    if value == 'primera_dosis_cantidad':
-        fig = px.bar(
-            data_frame = df,
-            x = 'jurisdiccion_nombre',
-            y = 'primera_dosis_cantidad')
-    else:
-        fig = px.bar(
-            data_frame= df,
-            x = 'jurisdiccion_nombre',
-            y = 'segunda_dosis_cantidad')
+    # Agregar las barras para cada categoría de 'purpose'
+    for purpose, data in filtered_df.groupby('purpose'):
+        fig.add_trace(go.Histogram(
+            x=data['purpose'],
+            y=data['credit_amount'],
+            name=str(purpose)
+            #marker_color="#02aaf8" if purpose == 'NO' else "#023368"
+        ))
+
+    # Configura el diseño del gráfico
+    fig.update_layout(
+        title="Distribución del Monto del Crédito por Propósito",
+        xaxis_title="Propósito",
+        yaxis_title="Monto del Crédito",
+        barmode='group',
+        bargap=0.1
+    )
+
     return fig
 
 @app.callback(
-    Output('pie_graph', component_property='figure'),
-    [Input('dosis-radioitems', component_property='value')])
-
+    Output('pie_graph', 'figure'),
+    [Input('credit_history', 'value')]
+)
 def update_graph_pie(value):
+    filtered_df = df[df['credit_history'] == value]
+    purpose_counts = filtered_df['purpose'].value_counts()
+    
+    fig2 = go.Figure()
 
-    if value == 'primera_dosis_cantidad':
-        fig2 = px.pie(
-            data_frame = df,
-            names = 'jurisdiccion_nombre',
-            values = 'primera_dosis_cantidad')
-    else:
-        fig2 = px.pie(
-            data_frame = df,
-            names = 'jurisdiccion_nombre',
-            values = 'segunda_dosis_cantidad'
-        )
+    fig2.add_trace(go.Pie(
+        labels=purpose_counts.index,
+        values=purpose_counts.values,
+        hole=0.2
+       # marker=dict(colors=["#02aaf8", "#023368", "#abcdef", "#123456"])  # Colores personalizados opcionales
+    ))
+
+    fig2.update_layout(
+        title=f'Distribución del Monto del Crédito por Propósito',
+        width=600,
+        height=400
+    )
+
     return fig2
 
-if __name__ == ('__main__'):
+if __name__ == '__main__':
     app.run_server(debug=True)
